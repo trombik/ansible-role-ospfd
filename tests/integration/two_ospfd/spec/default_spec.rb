ef9c4f328e529fe083c06012a4cf45fd56bce9a1
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-12-06 14:55:48 +0900

    [bugfix] Update rubocop to the latest

--- tests/integration/two_ospfd/spec/default_spec.rb
+++ tests/integration/two_ospfd/spec/default_spec.rb
@@ -11,7 +11,7 @@ context "after provisioning finished" do
       result = current_server.ssh_exec("ping -q -c 3 #{server(:r2).server.address} && echo OK")
       expect(result).to match(/OK/)
     end
-    it 'can reach to the peer\'s internal address with its own internal address' do
+    it "can reach to the peer's internal address with its own internal address" do
       result = current_server.ssh_exec("ping -q -c 3 -I 192.168.100.100 192.168.200.100 && echo OK")
       expect(result).to match(/OK/)
     end
@@ -22,7 +22,7 @@ context "after provisioning finished" do
       result = current_server.ssh_exec("ping -q -c 3 #{server(:r1).server.address} && echo OK")
       expect(result).to match(/OK/)
     end
-    it 'can reach to the peer\'s internal address with its own internal address' do
+    it "can reach to the peer's internal address with its own internal address" do
       result = current_server.ssh_exec("ping -q -c 3 -I 192.168.200.100 192.168.100.100 && echo OK")
       expect(result).to match(/OK/)
     end
